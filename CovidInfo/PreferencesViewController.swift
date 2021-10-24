857482a56096f79032eddec70b6ac944268b547b
zanspiler <zanspiler1@gmail.com>
2021-10-24 19:27:06 +0200

    basic fetching api data and displaying chart

---
+++ CovidInfo/PreferencesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  CovidInfo
+//
+//  Created by Zan Spiler on 24/10/2021.
+//
+
+import UIKit
+
+class PreferencesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+
+}
--- CovidInfo/ViewController.swift
+++ CovidInfo/ViewController.swift
@@ -6,14 +6,91 @@
 //
 
 import UIKit
+import Charts
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ChartViewDelegate {
+    
+    // Delegate - if user interacts with chart
+    
+    var lineChart = LineChartView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lineChart.delegate = self
+                
+        getData { (data) in
+            self.draw(data: data)
+        }
     }
+    
+    
+    private func getData(completion: @escaping ([HospitalData]) -> ()) {
 
+        URLSession.shared.dataTask(with: URL(string: "https://api.sledilnik.org/api/hospitals/")!, completionHandler: { [self] (data, response, error) in
+            guard let data = data, error == nil else {
+                // TODO - display error
+                return
+            }
+            
+            do {
+                let res = try! JSONDecoder().decode([HospitalData].self, from: data)
 
+                DispatchQueue.main.async {
+                    completion(res)
+                }
+                
+            } catch {
+                print("error: \(error.localizedDescription)")
+                // TODO: display error
+            }
+        }).resume()
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+//        self.draw(data)
+    }
+    
+    private func draw(data: [HospitalData]) {
+        lineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+
+        lineChart.center = view.center
+        view.addSubview(lineChart)
+
+        var entries = [ChartDataEntry]()
+
+        for (i, el) in data.enumerated() {
+            entries.append(ChartDataEntry(x: Double(i), y: Double(el.overall.icu.occupied)))
+        }
+
+        let set = LineChartDataSet(entries:entries)
+        set.colors = ChartColorTemplates.material()
+
+        lineChart.data = LineChartData(dataSet: set)
+        print("updated data")
+    }
+    
 }
+
+struct HospitalData: Codable {
+    let year: Int
+    let month: Int
+    let day: Int
+    let overall: OverallData
+}
+
+struct OverallData: Codable {
+    let beds: AmountData
+    let icu: AmountData
+    let vents: AmountData
+}
+
+struct AmountData: Codable {
+    let total: Int
+    let max: Int
+    let occupied: Int
+    let free: Int
+}
+
+
 
