9986ab1bad728208ca94f16addc2a13d776d27c3
zanspiler <zanspiler1@gmail.com>
2021-10-24 16:32:30 +0200

    Initial Commit

---
+++ CovidInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidInfo
+//
+//  Created by Zan Spiler on 24/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
