bb2fc822dee8e1c330bd8f93b1f799485144724d
zanspiler <zanspiler1@gmail.com>
2021-10-25 12:41:49 +0200

    Created hospitalizations chart

--- CovidInfo/ViewController.swift
+++ CovidInfo/ViewController.swift
@@ -26,14 +26,14 @@ class ViewController: UIViewController, ChartViewDelegate {
     
     private func getData(completion: @escaping ([HospitalData]) -> ()) {
 
-        URLSession.shared.dataTask(with: URL(string: "https://api.sledilnik.org/api/hospitals/")!, completionHandler: { [self] (data, response, error) in
+        URLSession.shared.dataTask(with: URL(string: "https://api.sledilnik.org/api/hospitals/")!, completionHandler: { (data, response, error) in
             guard let data = data, error == nil else {
                 // TODO - display error
                 return
             }
             
             do {
-                let res = try! JSONDecoder().decode([HospitalData].self, from: data)
+                let res = try JSONDecoder().decode([HospitalData].self, from: data)
 
                 DispatchQueue.main.async {
                     completion(res)
@@ -52,22 +52,115 @@ class ViewController: UIViewController, ChartViewDelegate {
     }
     
     private func draw(data: [HospitalData]) {
+        
+        // Init chart
         lineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
-
         lineChart.center = view.center
         view.addSubview(lineChart)
+        
+//        lineChart.backgroundColor = .systemGray3
+        lineChart.backgroundColor = .lightGray
 
-        var entries = [ChartDataEntry]()
+        lineChart.animate(xAxisDuration: 2.5)
+        
+        lineChart.rightAxis.enabled = false
+        
+        lineChart.xAxis.setLabelCount(6, force: true)
+        lineChart.xAxis.labelTextColor = .white
+        
+        lineChart.leftAxis.setLabelCount(6, force: false)
+        lineChart.leftAxis.labelTextColor = .white
+        lineChart.leftAxis.axisLineColor = .white
+        lineChart.leftAxis.labelPosition = .outsideChart
 
+        // 'Hospitalizirani' data
+        var hospiralizedData = [ChartDataEntry]()
         for (i, el) in data.enumerated() {
-            entries.append(ChartDataEntry(x: Double(i), y: Double(el.overall.icu.occupied)))
+            let hospitalized = Double(el.overall.icu.occupied + el.overall.beds.occupied)
+            hospiralizedData.append(ChartDataEntry(x: Double(i), y: hospitalized))
+        }
+    
+        let set = LineChartDataSet(entries: hospiralizedData)
+        set.label = "Hospitalizirani"
+        set.mode = .cubicBezier
+        set.cubicIntensity = 1
+        set.lineWidth = 2
+        set.drawCirclesEnabled = false
+        set.setColor(.systemBlue)
+
+        set.fillColor = .systemBlue
+        set.fillAlpha = 0.3
+        set.drawFilledEnabled = true;
+        set.drawHorizontalHighlightIndicatorEnabled = false
+
+        
+        // 'Intenzivna terapija' data
+        var icuData = [ChartDataEntry]()
+        for (i, el) in data.enumerated() {
+            icuData.append(ChartDataEntry(x: Double(i), y: Double(el.overall.icu.occupied)))
         }
 
-        let set = LineChartDataSet(entries:entries)
-        set.colors = ChartColorTemplates.material()
+        let set2 = LineChartDataSet(entries: icuData)
+        set2.mode = .cubicBezier
+        set2.label = "Intenzivna terapija"
+        set2.cubicIntensity = 1
+        set2.lineWidth = 2
+        set2.drawCirclesEnabled = false
+        set2.setColor(.systemRed)
 
-        lineChart.data = LineChartData(dataSet: set)
-        print("updated data")
+        set2.fillColor = .systemRed
+        set2.fillAlpha = 0.7
+        set2.drawFilledEnabled = true;
+        set2.drawHorizontalHighlightIndicatorEnabled = false
+
+        lineChart.data = LineChartData(dataSets: [set, set2])
+        
+    
+        // X-axis
+        lineChart.xAxis.labelPosition = .bottom
+        lineChart.xAxis.labelCount = set.count
+        lineChart.xAxis.drawLabelsEnabled = true
+        lineChart.xAxis.drawLimitLinesBehindDataEnabled = true
+        lineChart.xAxis.avoidFirstLastClippingEnabled = true
+       
+        var dates: [Date] = []
+        for el in data {
+            dates.append(createDate(day: el.day, month: el.month, year: el.year))
+        }
+                
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "sl")
+        dateFormatter.dateFormat = "dd MMM"
+        
+        lineChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: dates.map { dateFormatter.string(from: $0) })
+        lineChart.xAxis.setLabelCount(6, force: false)
+        
+        
+        lineChart.legend.textColor = .white
+                
+        // Create legend
+//        let hospLegendEntry = LegendEntry()
+//        hospLegendEntry.formColor = .white
+//        hospLegendEntry.label = "Hospitalizirani"
+//
+//        let icuLegendEntry = LegendEntry()
+//        icuLegendEntry.formColor = .systemRed
+//        icuLegendEntry.label = "Intenzivna terapija"
+//
+//        lineChart.legend.textColor = .white
+//        lineChart.legend.setCustom(entries: [hospLegendEntry, icuLegendEntry])
+    }
+    
+    private func createDate(day: Int, month: Int, year: Int) -> Date {
+        var dateComponents = DateComponents()
+        
+        dateComponents.day = day
+        dateComponents.month = month
+        dateComponents.year = year
+        dateComponents.timeZone = TimeZone(abbreviation: "GMT+1")
+        
+        let userCalendar = Calendar(identifier: .gregorian)
+        return userCalendar.date(from: dateComponents)!
     }
     
 }
