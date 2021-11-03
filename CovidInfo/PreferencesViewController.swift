48e75e689b81bf84ed7c16103e5c088048ba12ba
zanspiler <zanspiler1@gmail.com>
2021-11-03 18:17:53 +0100

    Added switches to preferences, displaying only user-set charts (by hidding cells via row height)

--- CovidInfo/PreferencesViewController.swift
+++ CovidInfo/PreferencesViewController.swift
@@ -2,9 +2,147 @@ import UIKit
 import Charts
 
 class PreferencesViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBOutlet weak var casesSwitch: UISwitch!
+    @IBOutlet weak var hospSwitch: UISwitch!
+    @IBOutlet weak var vaccSwitch: UISwitch!
+    @IBOutlet weak var deathsSwitch: UISwitch!
+    @IBOutlet weak var regionsSwitch: UISwitch!
+    @IBOutlet weak var vaccByAgeSwitch: UISwitch!
+    @IBOutlet weak var vaccPopularitySwitch: UISwitch!
+
+    
+    let userDefaults = UserDefaults.standard
+    
+    let CASES_SWITCH_KEY = "cases"
+    let HOSP_SWITCH_KEY = "hospitalizations"
+    let VACC_SWITCH_KEY = "vaccinations"
+    let DEATHS_SWITCH_KEY = "deaths"
+    let REGIONS_SWITCH_KEY = "regions"
+    let VACCBYAGE_SWITCH_KEY = "vaccinationsByAge"
+    let VACCPOPULARITY_SWITCH_KEY = "vaccinePopularity"
+    
+    
+    @IBAction func hospSwitchChanged(_ sender: Any) {
+        if (hospSwitch.isOn) {
+            userDefaults.set(true, forKey: HOSP_SWITCH_KEY)
+        } else {
+            userDefaults.set(false, forKey: HOSP_SWITCH_KEY)
+        }
+    }
+    
+    @IBAction func casesSwitchChanged(_ sender: Any) {
+        if (casesSwitch.isOn) {
+            userDefaults.set(true, forKey: CASES_SWITCH_KEY)
+        } else {
+            userDefaults.set(false, forKey: CASES_SWITCH_KEY)
+        }
+    }
+    
+    @IBAction func vaccSwitchChanged(_ sender: Any) {
+        if (vaccSwitch.isOn) {
+            userDefaults.set(true, forKey: VACC_SWITCH_KEY)
+        } else {
+            userDefaults.set(false, forKey: VACC_SWITCH_KEY)
+        }
+    }
+    
+    @IBAction func vaccPopularitySwitchChanged(_ sender: Any) {
+        if (vaccPopularitySwitch.isOn) {
+            userDefaults.set(true, forKey: VACCPOPULARITY_SWITCH_KEY)
+        } else {
+            userDefaults.set(false, forKey: VACCPOPULARITY_SWITCH_KEY)
+        }
+    }
+    
+    @IBAction func deathsSwitchChanged(_ sender: Any) {
+        if (deathsSwitch.isOn) {
+            userDefaults.set(true, forKey: DEATHS_SWITCH_KEY)
+        } else {
+            userDefaults.set(false, forKey: DEATHS_SWITCH_KEY)
+        }
+    }
+    
+    @IBAction func regionsSwitchChanged(_ sender: Any) {
+        if (regionsSwitch.isOn) {
+            userDefaults.set(true, forKey: REGIONS_SWITCH_KEY)
+        } else {
+            userDefaults.set(false, forKey: REGIONS_SWITCH_KEY)
+        }
         
     }
+    
+    @IBAction func vaccByAgeSwitchChanged(_ sender: Any) {
+        if (vaccByAgeSwitch.isOn) {
+            userDefaults.set(true, forKey: VACCBYAGE_SWITCH_KEY)
+        } else {
+            userDefaults.set(false, forKey: VACCBYAGE_SWITCH_KEY)
+        }
+    }
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkSwitchState()
+    }
+    
+ 
+    func checkSwitchState() {
+        if (userDefaults.bool(forKey: CASES_SWITCH_KEY)) {
+            casesSwitch.setOn(true, animated: false)
+        }
+        else {
+            casesSwitch.setOn(false, animated: true)
+        }
+        
+        if (userDefaults.bool(forKey: HOSP_SWITCH_KEY)) {
+            hospSwitch.setOn(true, animated: false)
+        }
+        else {
+            hospSwitch.setOn(false, animated: true)
+        }
+        
+        if (userDefaults.bool(forKey: VACC_SWITCH_KEY)) {
+            vaccSwitch.setOn(true, animated: false)
+        }
+        else {
+            vaccSwitch.setOn(false, animated: true)
+        }
+        
+        if (userDefaults.bool(forKey: DEATHS_SWITCH_KEY)) {
+            deathsSwitch.setOn(true, animated: false)
+        }
+        else {
+            deathsSwitch.setOn(false, animated: true)
+        }
+        
+        
+        if (userDefaults.bool(forKey: REGIONS_SWITCH_KEY)) {
+            regionsSwitch.setOn(true, animated: false)
+        }
+        else {
+            regionsSwitch.setOn(false, animated: true)
+        }
+        
+        
+        if (userDefaults.bool(forKey: VACCBYAGE_SWITCH_KEY)) {
+            vaccByAgeSwitch.setOn(true, animated: false)
+        }
+        else {
+            vaccByAgeSwitch.setOn(false, animated: true)
+        }
+        
+        if (userDefaults.bool(forKey: VACCPOPULARITY_SWITCH_KEY)) {
+            vaccPopularitySwitch.setOn(true, animated: false)
+        }
+        else {
+            vaccPopularitySwitch.setOn(false, animated: true)
+        }
+        
+        
+        
+    }
+    
+    
 }
 
--- CovidInfo/ViewController.swift
+++ CovidInfo/ViewController.swift
@@ -6,23 +6,41 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet var table: UITableView!
 
-    var charts = [Chart]()
+    public var charts = [Chart]()
+    var hidden = [false, false, false, false, false, false, false]
+    
+    let CASES_SWITCH_KEY = "cases"
+    let HOSP_SWITCH_KEY = "hospitalizations"
+    let VACC_SWITCH_KEY = "vaccinations"
+    let DEATHS_SWITCH_KEY = "deaths"
+    let REGIONS_SWITCH_KEY = "regions"
+    let VACCBYAGE_SWITCH_KEY = "vaccinationsByAge"
+    let VACCPOPULARITY_SWITCH_KEY = "vaccinePopularity"
+        
+    let userDefaults = UserDefaults.standard
+    
+    override func viewDidAppear(_ animated: Bool) {
+        checkUserPreferences()
+        self.table.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        checkUserPreferences()
         
         table.register(LineChartCell.nib(), forCellReuseIdentifier: LineChartCell.identifier)
         table.register(BarChartCell.nib(), forCellReuseIdentifier: BarChartCell.identifier)
 
         table.delegate = self
         table.dataSource = self
-    
+
+
         let placeholder = Chart(title: "", type: ChartType.Line, datasets: [], dates: [], labels:[])
-        
-        // TODO: display loading animtion in placeholders
+
+        // placeholder used to maintain chart order
         self.charts = [placeholder, placeholder, placeholder, placeholder, placeholder, placeholder, placeholder]
-        
-        
+
         // Display charts
         getCasesData() { (data) in
             self.charts[0] = data
@@ -44,28 +62,38 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             self.charts[3].type = ChartType.Bar
             self.table.reloadData()
         }
-        
+
         getDeathsData() { (data) in
             self.charts[4] = data
             self.table.reloadData()
         }
-        
+
         getVaccinationsByAgeData() { (data) in
             self.charts[5] = data
             self.charts[5].type = ChartType.Bar
             self.table.reloadData()
         }
-        
+
         getRegionsData() { (data) in
             self.charts[6] = data
             self.table.reloadData()
         }
-        
+
+            
 
     }
     
-
+    func checkUserPreferences() {
+        hidden[0] = userDefaults.bool(forKey: CASES_SWITCH_KEY) ? false : true
+        hidden[1] = userDefaults.bool(forKey: HOSP_SWITCH_KEY) ? false : true
+        hidden[2] = userDefaults.bool(forKey: VACC_SWITCH_KEY) ? false : true
+        hidden[3] = userDefaults.bool(forKey: DEATHS_SWITCH_KEY) ? false : true
+        hidden[4] = userDefaults.bool(forKey: REGIONS_SWITCH_KEY) ? false : true
+        hidden[5] = userDefaults.bool(forKey: VACCBYAGE_SWITCH_KEY) ? false : true
+        hidden[6] = userDefaults.bool(forKey: VACCPOPULARITY_SWITCH_KEY) ? false : true
+    }
     
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -73,11 +101,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return charts.count
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var rowHeight:CGFloat = 0.0
+        hidden[indexPath.row] ? (rowHeight = 0.0) : (rowHeight = UITableView.automaticDimension) //(rowHeight = 500.0)
+        return rowHeight
+    }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         if charts[indexPath.row].type == ChartType.Line {
             let cell = tableView.dequeueReusableCell(withIdentifier: LineChartCell.identifier, for: indexPath) as! LineChartCell
             cell.configure(with: charts[indexPath.row])
+            
+        
+            // Hide cell according to user settings
+            if (hidden[indexPath.row]) {
+                cell.isHidden = true
+            }
+            
             return cell
         }
         else {
